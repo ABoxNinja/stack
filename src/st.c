b063f977968c01becd4775d598af6d1e225f34e8
= <=>
2023-11-21 17:23:20 +0200

    added more commands

--- src/st.c
+++ src/st.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+char* profile;
 
 void nl()
 {
-    char* profile = getenv("USERPROFILE");
+    profile = getenv("USERPROFILE");
     const char* last_sep = strrchr(profile,'\\');
     printf("\033[1;32m-> \033[0m");
     if (last_sep != NULL)
@@ -30,6 +31,7 @@ void nl()
 
 int main()
 {
+    printf("\033[2J\033[H");
     nl();
     char input[64];
     while(1)
@@ -45,10 +47,35 @@ int main()
                 nbfr[nlp] = '\0';
             }
         
-            if (strcmp(input, "hello\n") == 0) {
-                printf("%s\n", nbfr);
+            if (strcmp(input, "ping\n") == 0) {
+                printf("pong\n");
                 nl();
-            }else if ((strcmp(input ,"\n"))==0)
+            }
+            else if (strcmp(input, "pong\n") == 0) {
+                printf("ping\n");
+                nl();
+            }
+            else if ((strcmp(input, "clear\n"))==0)
+            {
+                printf("\033[2J\033[H");
+                nl();
+            }
+            else if ((strcmp(input, "clean\n"))==0)
+            {
+                printf("\033[2J\033[H");
+                nl();
+            }
+            else if ((strcmp(input, "clean\n"))==0)
+            {
+                printf("\033[2J\033[H");
+                nl();
+            }
+            else if ((strcmp(input,"info\n"))==0)
+            {
+                printf("\e[0;34mStack terminal was made by \e[44mABoxNinja\e[0;34m on \e[1;34mGitHub\e[0m\n");
+                nl();
+            }
+            else if ((strcmp(input ,"\n"))==0)
             {
                 nl();
             }else {
